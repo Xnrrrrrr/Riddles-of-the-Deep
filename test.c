32e68505aee1e1c4fba37a4af592ed1b7bc43121
xnrrrrrr <xnrrrrrr@gmail.com>
2024-01-04 20:47:39 -0500

    added Islands + desc

--- test.c
+++ test.c
@@ -61,12 +61,12 @@ typedef struct {
     bool is_looted;
 } Island;
 
-Difficulty difficulty;
+Difficulty difficulty;      // declaring
 
-Attributes getBasePirateAttributes(Difficulty difficulty) {
-    Attributes baseAttributes;
+Attributes getBasePirateAttributes() {         // function
+    Attributes baseAttributes;                  // declaring baseAttributes
 
-    int thievery_base = 10 + rand() % 7; // 10 to 16
+    int thievery_base = 10 + rand() % 7; // 10 to 16        // assigns all baseAttributes to the pirates
     int charisma_base = 8 + rand() % 7; // 8 to 14
     int seamanship_base = 12 + rand() % 7; // 12 to 18
     int medicine_base = 6 + rand() % 7; // 6 to 12
@@ -74,7 +74,7 @@ Attributes getBasePirateAttributes(Difficulty difficulty) {
     int leadership_base = 10 + rand() % 7; // 10 to 16
     int carpentry_base = 9 + rand() % 7; // 9 to 15
 
-    switch (difficulty) {
+    switch (difficulty) {                               // switch case based on diff, that divides base attributes
         case EASY:
             baseAttributes.thievery = thievery_base;
             baseAttributes.charisma = charisma_base;
@@ -106,13 +106,13 @@ Attributes getBasePirateAttributes(Difficulty difficulty) {
             break;
     }
 
-    return baseAttributes;
+    return baseAttributes;      // returns the baseAttributes to the main
 }
 
-void initializeIslands(Island islands[], int numIslands) {
+void initializeIslands(Island islands[], int numIslands) {          // function that TAKES island array and numIslands to
     // Non-hostile islands
     snprintf(islands[0].name, MAX_STRING_LENGTH, "Tortuga");
-    snprintf(islands[0].description, MAX_STRING_LENGTH, "A lively pirate haven.");
+    snprintf(islands[0].description, MAX_STRING_LENGTH, "A lively pirate haven. Starting isle.");
     islands[0].treasure_available = 30;
     islands[0].max_treasure = 30;
     islands[0].food_available = 20;
@@ -124,21 +124,270 @@ void initializeIslands(Island islands[], int numIslands) {
     islands[0].is_island_hostile = false;
     islands[0].is_looted = false;
 
+    snprintf(islands[1].name, MAX_STRING_LENGTH, "Crimson Cove");
+    snprintf(islands[1].description, MAX_STRING_LENGTH, "Known for its vibrant red coral reefs");
+    islands[1].treasure_available = 30;
+    islands[1].max_treasure = 30;
+    islands[1].food_available = 20;
+    islands[1].max_food = 20;
+    islands[1].cannonballs_available = 10;
+    islands[1].max_cannonballs = 10;
+    islands[1].rum_available = 15;
+    islands[1].max_rum = 15;
+    islands[1].is_island_hostile = false;
+    islands[1].is_looted = false;
+
+    snprintf(islands[3].name, MAX_STRING_LENGTH, "Emerald Isle");
+    snprintf(islands[3].description, MAX_STRING_LENGTH, "Rumored to have mystical emerald artifacts");
+    islands[3].treasure_available = 30;
+    islands[3].max_treasure = 30;
+    islands[3].food_available = 20;
+    islands[3].max_food = 20;
+    islands[3].cannonballs_available = 10;
+    islands[3].max_cannonballs = 10;
+    islands[3].rum_available = 15;
+    islands[3].max_rum = 15;
+    islands[3].is_island_hostile = false;
+    islands[3].is_looted = false;
+
+    snprintf(islands[4].name, MAX_STRING_LENGTH, "Gold Rush Atoll");
+    snprintf(islands[4].description, MAX_STRING_LENGTH, "Once the sight of a legendary gold rush");
+    islands[4].treasure_available = 30;
+    islands[4].max_treasure = 30;
+    islands[4].food_available = 20;
+    islands[4].max_food = 20;
+    islands[4].cannonballs_available = 10;
+    islands[4].max_cannonballs = 10;
+    islands[4].rum_available = 15;
+    islands[4].max_rum = 15;
+    islands[4].is_island_hostile = false;
+    islands[4].is_looted = false;
+
+    snprintf(islands[5].name, MAX_STRING_LENGTH, "Whispering Sands");
+    snprintf(islands[5].description, MAX_STRING_LENGTH, "Desert Island with secrets carried by the wind");
+    islands[5].treasure_available = 30;
+    islands[5].max_treasure = 30;
+    islands[5].food_available = 20;
+    islands[5].max_food = 20;
+    islands[5].cannonballs_available = 10;
+    islands[5].max_cannonballs = 10;
+    islands[5].rum_available = 15;
+    islands[5].max_rum = 15;
+    islands[5].is_island_hostile = false;
+    islands[5].is_looted = false;
+
+    snprintf(islands[6].name, MAX_STRING_LENGTH, "Admiral's Archipelago");
+    snprintf(islands[6].description, MAX_STRING_LENGTH, "Enclave for Pirates of the highest status");
+    islands[6].treasure_available = 30;
+    islands[6].max_treasure = 30;
+    islands[6].food_available = 20;
+    islands[6].max_food = 20;
+    islands[6].cannonballs_available = 10;
+    islands[6].max_cannonballs = 10;
+    islands[6].rum_available = 15;
+    islands[6].max_rum = 15;
+    islands[6].is_island_hostile = false;
+    islands[6].is_looted = false;
+
+    snprintf(islands[7].name, MAX_STRING_LENGTH, "Port Royal Haven");
+    snprintf(islands[7].description, MAX_STRING_LENGTH, "Shrouded in mystery under the moonlight");
+    islands[7].treasure_available = 30;
+    islands[7].max_treasure = 30;
+    islands[7].food_available = 20;
+    islands[7].max_food = 20;
+    islands[7].cannonballs_available = 10;
+    islands[7].max_cannonballs = 10;
+    islands[7].rum_available = 15;
+    islands[7].max_rum = 15;
+    islands[7].is_island_hostile = false;
+    islands[7].is_looted = false;
+
+    snprintf(islands[8].name, MAX_STRING_LENGTH, "Nassau Harbor");
+    snprintf(islands[8].description, MAX_STRING_LENGTH, "Freedom port, pirate democracy, vibrant nightlife, notorious captains thrive");
+    islands[8].treasure_available = 30;
+    islands[8].max_treasure = 30;
+    islands[8].food_available = 20;
+    islands[8].max_food = 20;
+    islands[8].cannonballs_available = 10;
+    islands[8].max_cannonballs = 10;
+    islands[8].rum_available = 15;
+    islands[8].max_rum = 15;
+    islands[8].is_island_hostile = false;
+    islands[8].is_looted = false;
+
+    snprintf(islands[9].name, MAX_STRING_LENGTH, "Buccaneers Refuge");
+    snprintf(islands[9].description, MAX_STRING_LENGTH, "Sanctuary for outcasts, camaraderie, shared spoils, legendary pirate lore");
+    islands[9].treasure_available = 30;
+    islands[9].max_treasure = 30;
+    islands[9].food_available = 20;
+    islands[9].max_food = 20;
+    islands[9].cannonballs_available = 10;
+    islands[9].max_cannonballs = 10;
+    islands[9].rum_available = 15;
+    islands[9].max_rum = 15;
+    islands[9].is_island_hostile = false;
+    islands[9].is_looted = false;
+
+    snprintf(islands[10].name, MAX_STRING_LENGTH, "Gold Coast Retreat");
+    snprintf(islands[10].description, MAX_STRING_LENGTH, "Opulent haven, lavish lifestyles, treacherous alliances, high-stakes intrigues unfold.");
+    islands[10].treasure_available = 30;
+    islands[10].max_treasure = 30;
+    islands[10].food_available = 20;
+    islands[10].max_food = 20;
+    islands[10].cannonballs_available = 10;
+    islands[10].max_cannonballs = 10;
+    islands[10].rum_available = 15;
+    islands[10].max_rum = 15;
+    islands[10].is_island_hostile = false;
+    islands[10].is_looted = false;
+
+
     // Add more non-hostile islands as needed...
 
     // Hostile islands
-    snprintf(islands[1].name, MAX_STRING_LENGTH, "Isla de Muerta");
-    snprintf(islands[1].description, MAX_STRING_LENGTH, "A mysterious and dangerous island.");
-    islands[1].treasure_available = 10;
-    islands[1].max_treasure = 10;
-    islands[1].food_available = 5;
-    islands[1].max_food = 5;
-    islands[1].cannonballs_available = 20;
-    islands[1].max_cannonballs = 20;
-    islands[1].rum_available = 5;
-    islands[1].max_rum = 5;
-    islands[1].is_island_hostile = true;
-    islands[1].is_looted = false;
+    snprintf(islands[11].name, MAX_STRING_LENGTH, "Isla de Muerta");
+    snprintf(islands[11].description, MAX_STRING_LENGTH, "A mysterious and dangerous island.");
+    islands[11].treasure_available = 10;
+    islands[11].max_treasure = 10;
+    islands[11].food_available = 5;
+    islands[11].max_food = 5;
+    islands[11].cannonballs_available = 20;
+    islands[11].max_cannonballs = 20;
+    islands[11].rum_available = 5;
+    islands[11].max_rum = 5;
+    islands[11].is_island_hostile = true;
+    islands[11].is_looted = false;
+
+    snprintf(islands[12].name, MAX_STRING_LENGTH, "Skullcap Atoll");
+    snprintf(islands[12].description, MAX_STRING_LENGTH, "Dreaded prison, pirate executions, haunted waters, cursed shores beckon danger");
+    islands[12].treasure_available = 10;
+    islands[12].max_treasure = 10;
+    islands[12].food_available = 5;
+    islands[12].max_food = 5;
+    islands[12].cannonballs_available = 20;
+    islands[12].max_cannonballs = 20;
+    islands[12].rum_available = 5;
+    islands[12].max_rum = 5;
+    islands[12].is_island_hostile = true;
+    islands[12].is_looted = false;
+
+    snprintf(islands[13].name, MAX_STRING_LENGTH, "Dead Man's Reef");
+    snprintf(islands[13].description, MAX_STRING_LENGTH, "A mysterious and dangerous island, Ship graveyard, treacherous currents, ghostly whispers, cursed maritime history.");
+    islands[13].treasure_available = 10;
+    islands[13].max_treasure = 10;
+    islands[13].food_available = 5;
+    islands[13].max_food = 5;
+    islands[13].cannonballs_available = 20;
+    islands[13].max_cannonballs = 20;
+    islands[13].rum_available = 5;
+    islands[13].max_rum = 5;
+    islands[13].is_island_hostile = true;
+    islands[13].is_looted = false;
+
+    snprintf(islands[14].name, MAX_STRING_LENGTH, "Noose Point");
+    snprintf(islands[14].description, MAX_STRING_LENGTH, "Infamous for executions, pirate justice, a grim place of finality");
+    islands[14].treasure_available = 10;
+    islands[14].max_treasure = 10;
+    islands[14].food_available = 5;
+    islands[14].max_food = 5;
+    islands[14].cannonballs_available = 20;
+    islands[14].max_cannonballs = 20;
+    islands[14].rum_available = 5;
+    islands[14].max_rum = 5;
+    islands[14].is_island_hostile = true;
+    islands[14].is_looted = false;
+
+    snprintf(islands[15].name, MAX_STRING_LENGTH, "Red Tide Haven");
+    snprintf(islands[15].description, MAX_STRING_LENGTH, "Blood-soaked shores, notorious battles, relentless pirates, danger on every wave");
+    islands[15].treasure_available = 10;
+    islands[15].max_treasure = 10;
+    islands[15].food_available = 5;
+    islands[15].max_food = 5;
+    islands[15].cannonballs_available = 20;
+    islands[15].max_cannonballs = 20;
+    islands[15].rum_available = 5;
+    islands[15].max_rum = 5;
+    islands[15].is_island_hostile = true;
+    islands[15].is_looted = false;
+
+    snprintf(islands[16].name, MAX_STRING_LENGTH, "Calico's Cursed Island");
+    snprintf(islands[16].description, MAX_STRING_LENGTH, "Infested by pirate outcasts, betrayal, buried grudges, an island of strife");
+    islands[16].treasure_available = 10;
+    islands[16].max_treasure = 10;
+    islands[16].food_available = 5;
+    islands[16].max_food = 5;
+    islands[16].cannonballs_available = 20;
+    islands[16].max_cannonballs = 20;
+    islands[16].rum_available = 5;
+    islands[16].max_rum = 5;
+    islands[16].is_island_hostile = true;
+    islands[16].is_looted = false;
+
+    snprintf(islands[17].name, MAX_STRING_LENGTH, "Porto Desperation Isle");
+    snprintf(islands[17].description, MAX_STRING_LENGTH, "Exile's refuge, banished pirates, desperate actions, a last resort");
+    islands[17].treasure_available = 10;
+    islands[17].max_treasure = 10;
+    islands[17].food_available = 5;
+    islands[17].max_food = 5;
+    islands[17].cannonballs_available = 20;
+    islands[17].max_cannonballs = 20;
+    islands[17].rum_available = 5;
+    islands[17].max_rum = 5;
+    islands[17].is_island_hostile = true;
+    islands[17].is_looted = false;
+
+    snprintf(islands[18].name, MAX_STRING_LENGTH, "Tunis");
+    snprintf(islands[18].description, MAX_STRING_LENGTH, "Desolate marsh isle, treacherous betrayal, festering vendettas, a realm gripped by strife");
+    islands[18].treasure_available = 10;
+    islands[18].max_treasure = 10;
+    islands[18].food_available = 5;
+    islands[18].max_food = 5;
+    islands[18].cannonballs_available = 20;
+    islands[18].max_cannonballs = 20;
+    islands[18].rum_available = 5;
+    islands[18].max_rum = 5;
+    islands[18].is_island_hostile = true;
+    islands[18].is_looted = false;
+
+    snprintf(islands[19].name, MAX_STRING_LENGTH, "Barataria Bay");
+    snprintf(islands[19].description, MAX_STRING_LENGTH, "Treacherous stronghold, pirate vendettas,");
+    islands[19].treasure_available = 10;
+    islands[19].max_treasure = 10;
+    islands[19].food_available = 5;
+    islands[19].max_food = 5;
+    islands[19].cannonballs_available = 20;
+    islands[19].max_cannonballs = 20;
+    islands[19].rum_available = 5;
+    islands[19].max_rum = 5;
+    islands[19].is_island_hostile = true;
+    islands[19].is_looted = false;
+
+    snprintf(islands[20].name, MAX_STRING_LENGTH, "Blackbeard's Abyss Isle");
+    snprintf(islands[20].description, MAX_STRING_LENGTH, "Shrouded cove, ominous legends, a feared and shadowed retreat");
+    islands[20].treasure_available = 10;
+    islands[20].max_treasure = 10;
+    islands[20].food_available = 5;
+    islands[20].max_food = 5;
+    islands[20].cannonballs_available = 20;
+    islands[20].max_cannonballs = 20;
+    islands[20].rum_available = 5;
+    islands[20].max_rum = 5;
+    islands[20].is_island_hostile = true;
+    islands[20].is_looted = false;
+
+    snprintf(islands[21].name, MAX_STRING_LENGTH, "Corsair Bastion");
+    snprintf(islands[21].description, MAX_STRING_LENGTH, "Pirate stronghold, ceaseless raids, enslaved shores, echoes of lawlessness");
+    islands[21].treasure_available = 10;
+    islands[21].max_treasure = 10;
+    islands[21].food_available = 5;
+    islands[21].max_food = 5;
+    islands[21].cannonballs_available = 20;
+    islands[21].max_cannonballs = 20;
+    islands[21].rum_available = 5;
+    islands[21].max_rum = 5;
+    islands[21].is_island_hostile = true;
+    islands[21].is_looted = false;
+
 
     // Add more hostile islands as needed...
 
